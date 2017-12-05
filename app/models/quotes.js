f3ce08f27e36369c49fa7832f91e50ad1122c575
ALPESH PATEL <alpeshrpatel@gmail.com>
2017-12-05 15:18:43 -0700

    added commit in model

--- app/models/quotes.js
+++ app/models/quotes.js
@@ -4,6 +4,6 @@ var mongoose = require('mongoose');
 // define our nerd model
 // module.exports allows us to pass this to other files when it is called
 module.exports = mongoose.model('Quote', {
-	author : {type : String, default: ''}
+	author : {type : String, default: ''},
     quote  : {type : String, default: ''}
 });
