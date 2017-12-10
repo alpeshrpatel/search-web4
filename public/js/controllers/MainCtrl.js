be35fc0d2f50cc7ce92d3ed77f6ed28b1a0dc7a4
ALPESH PATEL <alpeshrpatel@gmail.com>
2017-12-10 10:39:10 -0700

    Adding header service and get data dynamically from mongo

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,11 +1,34 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope,$http, $routeParams) {
-
+	
+	var col = 3;
 	$scope.tagline = 'To the moon and back!';	
 	$scope.isaddschool=false;
 	$scope.issearchdata = false;
 	$scope.schoollist = null;
 	console.log("issearchdata : " + $scope.issearchdata );
-	
+	//$scope.loadHeader();
+	$http({
+		  method: 'GET',
+		  url: 'http://localhost:4000/api/headers'
+		}).then(function successCallback(response) {
+			$scope.headerlist = response.data;
+			var headertable = [];
+			var headerrow = [];
+			for(var i=0; i < $scope.headerlist.length ; i++)
+			{
+				if(i%col==0){
+					headerrow = [];
+					headertable.push(headerrow);
+				}
+				headerrow.push($scope.headerlist[i]);
+				//console.log(JSON.stringify($scope.headerlist[i]));
+			}
+			console.log(headertable);
+			$scope.headertable = headertable;
+		  }, function errorCallback(response) {
+			  $scope.response=response;
+			  console.log("Please check erorr log or console : Header service error");
+		  });
 	
 	$scope.onClickGo = function(){
 		console.log('On Click Go');
@@ -23,7 +46,7 @@ angular.module('MainCtrl', []).controller('MainController', function($scope,$htt
 				//console.log("Records :" + JSON.stringify($scope.schoollist));
 			  }, function errorCallback(response) {
 				  $scope.response=response;
-				  console.log("Please check erorr log or console");
+				  console.log("Please check erorr log or console :School Service");
 			  });
 	}
 	
@@ -32,6 +55,19 @@ angular.module('MainCtrl', []).controller('MainController', function($scope,$htt
 		$scope.isaddschool=false;
 		$scope.issearchdata = false;
 		console.log("issearchdata : " + issearchdata );
+	}
+	
+	$scope.loadHeader = function(){
+		$http({
+			  method: 'GET',
+			  url: 'http://localhost:4000/api/headers'
+			}).then(function successCallback(response) {
+				$scope.headerlist = response.data;
+				console.log(JSON.stringify($scope.headerlist));
+			  }, function errorCallback(response) {
+				  $scope.response=response;
+				  console.log("Please check erorr log or console : Header service error");
+			  });
 	}
 	
 });
