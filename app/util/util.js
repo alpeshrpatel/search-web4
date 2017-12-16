3c56acd7a8dd4672b631973340032b6e3f517ce0
ALPESH PATEL <alpeshrpatel@gmail.com>
2017-12-16 15:48:32 -0700

    minor fix

--- app/util/util.js
+++ app/util/util.js
@@ -1,9 +1,14 @@
 
 
-function getRandomNumber(min, max) {
+module.exports.getRandomNumber =function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
-console.log(getRandomNumber(1,10));
-module.exports = getRandomNumber;
+module.exports.getDeviceDetails = function(req){
+	var devicePayload ={};
+	devicePayload.name = req.headers['user-agent'].toLowerCase();
+	devicePayload.type = req.headers['user-agent'].toLowerCase();
+	devicePayload.httpversion = req.headers['user-agent'].toLowerCase();
+	return devicePayload;
+}
