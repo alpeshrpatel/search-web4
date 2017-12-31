679eca36edd0c94b952674d0be659cbe0e4fad38
ALPESH PATEL <alpeshrpatel@gmail.com>
2017-12-31 10:39:53 -0700

    changing localhost to ip address

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -9,7 +9,7 @@ angular.module('MainCtrl', []).controller('MainController', function($scope,$htt
 	//$scope.loadHeader();
 	$http({
 		  method: 'GET',
-		  url: 'http://localhost:4000/api/headers'
+		  url: 'http://104.197.215.38:4000/api/headers'
 		}).then(function successCallback(response) {
 			$scope.headerlist = response.data;
 			var headertable = [];
